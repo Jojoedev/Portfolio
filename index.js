cc0774f7c6f1af30bd56b3ea85370e5e155c8ae7
Jojoedev <odogwujonas@gmail.com>
2022-04-02 21:17:03 +0100

    Add files via upload
    Updated Portfolio

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+//var today = new Date()
+//hour = today.getHours();
+//min = today.getMinutes();
+//sec = today.getSeconds();
+//timeNow =(`${hour}:${min}:${sec}`);
+/*
+var today = new Date()
+hour = today.getHours();
+let timeOfDay
+if(hour >= 12 && hour <= 17){
+	timeOfDay = "afternoon";
+ }
+ else if (hour >= 17) {
+ 	timeOfDay = "evening";
+ }
+
+ else if(hour >= 0 && hour < 12) {
+ 	timeOfDay = "morning";
+}
+
+greeting.innerHTML=`Good ${timeOfDay}!`
+
+setTimeout(function(){
+greeting.style.color="green"
+}, 8000)
+
+*/
+
+ 
